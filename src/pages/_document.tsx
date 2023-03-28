7675cf990fe528b128f20cbcd99f37d849ce6aea
RedDTG <a.deyris85@gmail.com>
2023-03-29 00:28:55 +0200

    hotfix: components folder outside pages folder

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { Header } from './components/header'
-import { Footer } from './components/footer'
+import { Header } from '../components/header'
+import { Footer } from '../components/footer'
 
 export default function Document() {
   return (
